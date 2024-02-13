e3cb5bfc02324bae3d124bf49f78b0480ec9a517
asadbe0304 <asadbekaxmadkulov@gmail.com>
2024-02-13 21:41:38 +0500

    Dashboard Layout

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashBoardPage = () => {
+  return (
+    <>
+      Dashboard Page
+    </>
+  );
+};
+
+export default DashBoardPage;
--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,7 +4,6 @@ export const Loading = () => {
   return (
     <div className="h-full w-full flex flex-col gap-y-4 justify-center items-center">
       <Image src={'./logo.svg'} alt="logo" width={120} height={120} className="animate-pulse duration-700" />
-
     </div>
   )
 }
